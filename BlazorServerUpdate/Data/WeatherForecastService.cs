6a8a34904e223c0c5a1ff29c135974212a196417
Eero <49983218+eero-dev@users.noreply.github.com>
2021-12-09 03:14:08 +0200

    Enabled the new NET 6 analyzers and fixed warnings

--- BlazorServerUpdate/Data/WeatherForecastService.cs
+++ BlazorServerUpdate/Data/WeatherForecastService.cs
@@ -5,18 +5,18 @@ namespace BlazorServerUpdate.Data
         /// <summary>
         /// Event to notify frontend about data change
         /// </summary>
-        public event Action OnDataChange;
+        public event Action? OnDataChange;
         /// <summary>
         /// Method to call event
         /// </summary>
         private void NotifyDataChanged() => OnDataChange?.Invoke();
-        private static Timer DataFetcher { get; set; }
+        private static Timer? DataFetcher { get; set; }
         private static int CheckInterval { get; set; } = 100;
 
         //Default Blazor server stuff
         public static Random Rng { get; set; } = new Random();
 
-        private static List<string> Summaries = new List<string>
+        private static List<string> Summaries { get; } = new List<string>
         {
             "Freezing"
         };
@@ -36,23 +36,14 @@ namespace BlazorServerUpdate.Data
         /// Fetch data from somewhere
         /// </summary>
         /// <param name="sender"></param>
-        private void FetchData(object sender)
+        private void FetchData(object? sender)
         {
-            try
+            if (1 == 1) // Would be custom logic to determine if something has been updated.. 
             {
-
-                if (1 == 1) // Would be custom logic to determine if something has been updated.. 
-                {
-                    Summaries.Add(Guid.NewGuid().ToString("n").Substring(0, 8));
-                    NotifyDataChanged(); // Trigger event -> 'data has changed'
-                }
-
-                DataFetcher.Change(CheckInterval, Timeout.Infinite); // Trigger timer again
+                Summaries.Add(Guid.NewGuid().ToString("n")[..8]);
+                NotifyDataChanged(); // Trigger event -> 'data has changed'
             }
-            catch (Exception ex)
-            {
-                DataFetcher.Change(CheckInterval, Timeout.Infinite);  // Trigger timer again
-            }
+            DataFetcher?.Change(CheckInterval, Timeout.Infinite); // Trigger timer again
         }
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
